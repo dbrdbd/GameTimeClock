47bc692030601161746afcbf6ebb34d1be7b8ba0
dbrdbd <mail@shaynamehta.com>
2016-09-17 04:53:47 -0400

    Super basic prototype working
    Time Picker functionality - needs sound and backend

--- app/src/main/java/com/shaynamehta/gametimeclock/MainActivity.java
+++ app/src/main/java/com/shaynamehta/gametimeclock/MainActivity.java
@@ -1,13 +1,29 @@
 package com.shaynamehta.gametimeclock;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.PersistableBundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TimePicker timePicker1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timePicker1 = (TimePicker)findViewById(R.id.timePicker);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 }
