7a0e7e0d70c2d47a483086458733f574d09f9929
Andrew Chen <chenan02@gmail.com>
2016-09-17 12:34:41 -0400

    Get alarm clock working

---
+++ app/src/main/java/com/shaynamehta/gametimeclock/TimePair.java
@@ -0,0 +1,12 @@
+package com.shaynamehta.gametimeclock;
+
+import android.content.Context;
+
+/**
+ * Created by Andrew on 9/17/16.
+ */
+public class TimePair {
+    public Context context;
+    public int hour;
+    public int minute;
+}
